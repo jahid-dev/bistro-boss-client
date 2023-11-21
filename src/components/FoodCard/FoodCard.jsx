b2e448a035385dcddbb45a1ba31e3828ee13d7df
zinolot <mrjahidhassan0@gmail.com>
2023-11-22 01:42:42 +0600

    client connect to the server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -7,11 +7,11 @@ const FoodCard = ({ item }) => {
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={image} alt="Shoes" /></figure>
                 <p className="absolute right-0 text-white bg-slate-900 mr-4 px-4">${price}</p>
-                <div className="card-body">
+                <div className="card-body flex flex-col items-center">
                     <h2 className="card-title">{name}</h2>
                     <p>{recipe }</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">ADD TO CART</button>
+                        <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">ADD TO CART</button>
                     </div>
                 </div>
             </div>
